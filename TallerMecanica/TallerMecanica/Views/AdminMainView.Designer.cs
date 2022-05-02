738d4877f8e846588885b2c2f69a3e1eb4df2dd2
PedroBAlibrea <104756855+PedroBAlibrea@users.noreply.github.com>
2022-05-02 10:19:56 +0200

    cambio

--- TallerMecanica/TallerMecanica/Views/AdminMainView.Designer.cs
+++ TallerMecanica/TallerMecanica/Views/AdminMainView.Designer.cs
@@ -35,10 +35,10 @@ namespace TallerMecanica.Views
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(458, 285);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(611, 351);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,19 +46,21 @@ namespace TallerMecanica.Views
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(108, 83);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.Size = new System.Drawing.Size(258, 164);
             this.listBox1.TabIndex = 1;
             // 
             // AdminMainView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AdminMainView";
             this.Text = "AdminMainView";
             this.ResumeLayout(false);
