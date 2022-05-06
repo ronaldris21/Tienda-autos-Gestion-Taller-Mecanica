e047b6306cb9958f8725a6a75364d695508606c7
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2022-05-06 03:58:38 -0600

    Mostrar Clientes Registrados

--- TallerMecanica/TallerMecanica/Views/AdminViews/ClientesView.Designer.cs
+++ TallerMecanica/TallerMecanica/Views/AdminViews/ClientesView.Designer.cs
@@ -30,25 +30,105 @@ namespace TallerMecanica.Views.AdminViews
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefono1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefono2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isAdmin = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(363, 214);
+            this.button1.Location = new System.Drawing.Point(346, 394);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Clientes";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idCliente,
+            this.nombre,
+            this.Email,
+            this.Telefono1,
+            this.Telefono2,
+            this.pass,
+            this.isAdmin});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(743, 346);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // idCliente
+            // 
+            this.idCliente.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.idCliente.HeaderText = "id";
+            this.idCliente.Name = "idCliente";
+            this.idCliente.ReadOnly = true;
+            this.idCliente.Width = 40;
+            // 
+            // nombre
+            // 
+            this.nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // Email
+            // 
+            this.Email.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            this.Email.Width = 57;
+            // 
+            // Telefono1
+            // 
+            this.Telefono1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Telefono1.HeaderText = "Telefono 1";
+            this.Telefono1.Name = "Telefono1";
+            this.Telefono1.Width = 83;
+            // 
+            // Telefono2
+            // 
+            this.Telefono2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Telefono2.HeaderText = "Telefono 2";
+            this.Telefono2.Name = "Telefono2";
+            this.Telefono2.Width = 83;
+            // 
+            // pass
+            // 
+            this.pass.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.pass.HeaderText = "Contraseña";
+            this.pass.Name = "pass";
+            this.pass.Width = 86;
+            // 
+            // isAdmin
+            // 
+            this.isAdmin.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.isAdmin.HeaderText = "Administrador";
+            this.isAdmin.Name = "isAdmin";
+            this.isAdmin.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.isAdmin.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.isAdmin.Width = 95;
             // 
             // ClientesView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "ClientesView";
             this.Text = "Registro de clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +136,13 @@ namespace TallerMecanica.Views.AdminViews
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pass;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn isAdmin;
     }
 }
