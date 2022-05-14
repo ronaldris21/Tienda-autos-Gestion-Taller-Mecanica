1ad18f73aa882a94a0b7ebb3eaa8c87a39a47dc2
alexgelde <agelde@alu.ucam.edu>
2022-05-14 17:35:46 +0200

    aaa

--- TallerMecanica/TallerMecanica/Views/SignupView.Designer.cs
+++ TallerMecanica/TallerMecanica/Views/SignupView.Designer.cs
@@ -39,54 +39,66 @@ namespace TallerMecanica.Views
             this.tbemail = new System.Windows.Forms.TextBox();
             this.tbtelefono = new System.Windows.Forms.TextBox();
             this.tbcontraseña = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(183, 292);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(97)))), ((int)(((byte)(238)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(110, 379);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(256, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "Signup";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(152, 23);
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(19, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 19);
+            this.label1.Size = new System.Drawing.Size(90, 19);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre Completo";
+            this.label1.Text = "Full name:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(190, 89);
+            this.label2.ForeColor = System.Drawing.Color.LightGray;
+            this.label2.Location = new System.Drawing.Point(19, 113);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 19);
+            this.label2.Size = new System.Drawing.Size(56, 19);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Email";
+            this.label2.Text = "Email:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(181, 155);
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(198, 113);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.Size = new System.Drawing.Size(128, 19);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Telefono";
+            this.label3.Text = "Phone number:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(180, 224);
+            this.label4.ForeColor = System.Drawing.Color.LightGray;
+            this.label4.Location = new System.Drawing.Point(111, 219);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(98, 19);
             this.label4.TabIndex = 4;
@@ -95,61 +107,84 @@ namespace TallerMecanica.Views
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(108, 292);
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(94)))), ((int)(((byte)(129)))));
+            this.label5.Location = new System.Drawing.Point(339, 325);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(0, 13);
             this.label5.TabIndex = 5;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // tbnombre
             // 
-            this.tbnombre.Location = new System.Drawing.Point(142, 53);
+            this.tbnombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(94)))), ((int)(((byte)(129)))));
+            this.tbnombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbnombre.Location = new System.Drawing.Point(23, 68);
+            this.tbnombre.Multiline = true;
             this.tbnombre.Name = "tbnombre";
-            this.tbnombre.Size = new System.Drawing.Size(159, 20);
+            this.tbnombre.Size = new System.Drawing.Size(303, 26);
             this.tbnombre.TabIndex = 6;
             // 
             // tbemail
             // 
-            this.tbemail.Location = new System.Drawing.Point(168, 110);
+            this.tbemail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(94)))), ((int)(((byte)(129)))));
+            this.tbemail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbemail.Location = new System.Drawing.Point(23, 135);
+            this.tbemail.Multiline = true;
             this.tbemail.Name = "tbemail";
-            this.tbemail.Size = new System.Drawing.Size(100, 20);
+            this.tbemail.Size = new System.Drawing.Size(131, 20);
             this.tbemail.TabIndex = 7;
             // 
             // tbtelefono
             // 
-            this.tbtelefono.Location = new System.Drawing.Point(168, 185);
+            this.tbtelefono.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(94)))), ((int)(((byte)(129)))));
+            this.tbtelefono.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbtelefono.Location = new System.Drawing.Point(202, 135);
+            this.tbtelefono.Multiline = true;
             this.tbtelefono.Name = "tbtelefono";
-            this.tbtelefono.Size = new System.Drawing.Size(100, 20);
+            this.tbtelefono.Size = new System.Drawing.Size(124, 20);
             this.tbtelefono.TabIndex = 8;
             // 
             // tbcontraseña
             // 
-            this.tbcontraseña.Location = new System.Drawing.Point(168, 245);
+            this.tbcontraseña.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(94)))), ((int)(((byte)(129)))));
+            this.tbcontraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbcontraseña.Location = new System.Drawing.Point(50, 241);
+            this.tbcontraseña.Multiline = true;
             this.tbcontraseña.Name = "tbcontraseña";
             this.tbcontraseña.PasswordChar = '*';
-            this.tbcontraseña.Size = new System.Drawing.Size(100, 20);
+            this.tbcontraseña.Size = new System.Drawing.Size(239, 20);
             this.tbcontraseña.TabIndex = 9;
             this.tbcontraseña.UseSystemPasswordChar = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbtelefono);
+            this.panel1.Controls.Add(this.tbcontraseña);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbemail);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tbnombre);
+            this.panel1.Location = new System.Drawing.Point(60, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(351, 314);
+            this.panel1.TabIndex = 10;
             // 
             // SignupView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(59)))), ((int)(((byte)(104)))));
-            this.ClientSize = new System.Drawing.Size(482, 341);
-            this.Controls.Add(this.tbcontraseña);
-            this.Controls.Add(this.tbtelefono);
-            this.Controls.Add(this.tbemail);
-            this.Controls.Add(this.tbnombre);
+            this.ClientSize = new System.Drawing.Size(449, 475);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SignupView";
-            this.Text = "SignupView";
             this.Load += new System.EventHandler(this.SignupView_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,5 +202,6 @@ namespace TallerMecanica.Views
         private System.Windows.Forms.TextBox tbemail;
         private System.Windows.Forms.TextBox tbtelefono;
         private System.Windows.Forms.TextBox tbcontraseña;
+        private System.Windows.Forms.Panel panel1;
     }
 }
